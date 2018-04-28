9f0b7b9e541d49b52e8636e1329d201f723d7b87
DerGut <jannik.steinmann@gmx.de>
2018-04-28 18:01:22 +0200

    Removes unused import

--- StundenzettelCreator/urls.py
+++ StundenzettelCreator/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
-from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('creator.urls'))
