21ce2e1f4e0bfbcd21e7ede4b49911d742688552
DerGut <jannik.steinmann@gmx.de>
2018-05-28 14:05:01 +0200

    Adds more logging to the settings file. Closes #21

--- StundenzettelCreator/settings.py
+++ StundenzettelCreator/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+import logging
 import os
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,23 +24,26 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+if not SECRET_KEY:
+    logger.critical("No environment variable SECRET_KEY defined. "
+                    "See https://docs.djangoproject.com/en/1.11/ref/settings/#std:setting-SECRET_KEY "
+                    "for further information")
+    exit(-1)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False) == 'True'
-
-import logging
-# if DEBUG:
-#     logging.basicConfig(level=logging.DEBUG)
-# else:
-logging.basicConfig(level=logging.INFO)
-
+PRODUCTION = os.environ.get('PRODUCTION', False) == 'True'
+DEBUG = not PRODUCTION
 
 # Error notification
-ADMINS = [(os.environ.get('ADMIN_NAME'), os.environ.get('ADMIN_EMAIL'))]
-MANAGERS = [(os.environ.get('ADMIN_NAME'), os.environ.get('ADMIN_EMAIL'))]
+if not os.environ.get('ADMIN_NAME') and not os.environ.get('ADMIN_EMAIL'):
+    logger.error("Environment variables ADMIN_NAME and ADMIN_EMAIL "
+                 "are not set. These are important for receiving emails about "
+                 "server errors and messages from the contact page")
+else:
+    ADMINS = [(os.environ.get('ADMIN_NAME'), os.environ.get('ADMIN_EMAIL'))]
+    MANAGERS = [(os.environ.get('ADMIN_NAME'), os.environ.get('ADMIN_EMAIL'))]
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -81,7 +88,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'StundenzettelCreator.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -91,7 +97,6 @@ DATABASES = {
         'NAME': 'timesheets'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -111,7 +116,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -125,7 +129,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
@@ -133,25 +136,32 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Email
-if os.environ.get('DEBUG', False):
+if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
-    EMAIL_HOST = 'smtp.sendgrid.net'
-    EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
-    EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
+    if not os.environ.get('SENDGRID_USERNAME') and not os.environ.get('SENDGRID_PASSWORD'):
+        logger.error("The environment variables SENDGRID_USERNAME and SENDGRID_PASSWORD "
+                     "are not set. Please provision a free sendgrid add-on. Otherwise the "
+                     "email subscription and contact features will not function properly.")
+        EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    else:
+        EMAIL_HOST = 'smtp.sendgrid.net'
+        EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
+        EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
+        EMAIL_PORT = 587
+        EMAIL_USE_TLS = True
 
-# Deploy stuff
-SECURE_BROWSER_XSS_FILTER = True
-X_FRAME_OPTIONS = 'DENY'
+if PRODUCTION:
+    # Deploy stuff
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = 'DENY'
 
-# Activate Django-Heroku.
-if os.environ.get('HEROKU', False):
+    # Activate Django-Heroku.
     import django_heroku
+
     django_heroku.settings(locals())
 
 if DEBUG:
-    HOST_NAME = '127.0.0.1:8000'
+    HOST_NAME = 'http://127.0.0.1:8000'
 else:
     HOST_NAME = 'http://www.stundenzettel-creator.xyz'
